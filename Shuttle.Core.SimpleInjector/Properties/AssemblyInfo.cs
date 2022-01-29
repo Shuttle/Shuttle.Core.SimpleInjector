6129a069af60d987380d827fe7a2d081a67ec698
Eben <me@ebenroux.co.za>
2022-01-29 11:24:21 +0200

    - package upgrade

--- Shuttle.Core.SimpleInjector/Properties/AssemblyInfo.cs
+++ Shuttle.Core.SimpleInjector/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.6.0")]
+[assembly: AssemblyVersion("10.1.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Core.SimpleInjector")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.6")]
+[assembly: AssemblyInformationalVersion("10.1.0")]
 [assembly: ComVisible(false)]
